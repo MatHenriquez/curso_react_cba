86a0c1a4737e0aa8037fd420e9ce28aec22e138d
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-11-11 23:31:15 -0300

    Render the options for each Encuesta

--- clase_04/actividad_encuestas_app/src/components/Encuestas.jsx
+++ clase_04/actividad_encuestas_app/src/components/Encuestas.jsx
@@ -4,10 +4,18 @@ function Encuestas({ encuestas }) {
   return (
     <div>
       <h2>Encuestas Disponibles</h2>
-      <ul>
+      <ul className={encuestas}>
         {encuestas.map((encuesta) => (
-          <li key={encuesta.id}>{encuesta.pregunta}</li>
+          <li key={encuesta.id}>
+            {encuesta.pregunta}
+            <ul>
+              {encuesta.opciones.map((opcion) => (
+                <li key={opcion.id}>{opcion}</li>
+              ))}
+            </ul>
+          </li>
         ))}
+        <ul className="background-color: red;"></ul>
       </ul>
     </div>
   );
