f0b49bd6e3fe3967aed961d18d98ab96578a7b8a
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-11-11 23:30:49 -0300

    Add the 'Encuestas' in the landing page

--- clase_04/actividad_encuestas_app/src/App.js
+++ clase_04/actividad_encuestas_app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// Componentes de las páginas
+import Encuestas from "./components/Encuestas";
 
 function App() {
+  // Estado para rastrear las encuestas
+  const encuestasDeEjemplo = [
+    {
+      id: 1,
+      pregunta: "¿Cuál es tu color favorito?",
+      opciones: ["Rojo", "Azul", "Verde"],
+    },
+    {
+      id: 2,
+      pregunta: "¿Cuál es tu comida favorita?",
+      opciones: ["Pizza", "Hamburguesa", "Sushi"],
+    },
+  ];
+
+  const [encuestas] = useState(encuestasDeEjemplo);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Aplicación de Encuestas</h1>
+      {/* Contenido de las páginas */}
+      <Encuestas encuestas={encuestas} />
     </div>
   );
 }
-
 export default App;
